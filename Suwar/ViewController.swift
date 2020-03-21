a9ff1a8a6dd4f11b2f7cf104181d40b64bff3437
ilyasa Azmi <theazmi99ilyasa@gmail.com>
2020-03-21 14:29:09 +0700

    Initial Commit

---
+++ Suwar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Suwar
+//
+//  Created by Ilyasa Azmi on 21/03/20.
+//  Copyright © 2020 Ilyasa Azmi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
